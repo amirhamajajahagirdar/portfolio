7ec9e7fb4328fb2faf5bc3c7f91c86f337c7e243
Amirhamaja <amirhamajajahagirdar@gmail.com>
2026-07-14 12:11:06 +0530

    Add files via upload
    updated links as dynamic using js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const portfolio ={
+    name:"Amirhamaja Jahagirdar",
+    github: "https://github.com/amirhamajajahagirdar",
+    youtube: "https://youtube.com/@NextGenCoder-u6y",
+    instagram:"https://instagram.com/nextgencoder_422",
+    email: "mailto:nextgencoder422@gmail.com"
+}
